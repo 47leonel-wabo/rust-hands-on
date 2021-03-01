3fda3a954d38060e6e242fb7cbd274491d85be4a
leonel47 <waboleonel@gmail.com>
2021-03-01 11:11:08 -0500

    Defining struct

--- data_structre/src/main.rs
+++ data_structre/src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    let leo = Person {
+        username: String::from("leo ka"),
+        email: String::from("leoka@mymail.xyz"),
+        age: 28,
+        is_active: true,
+    };
+
+    let lea = Person {
+        username: String::from("lea Ny"),
+        age: 22,
+        ..leo // Fill the remaining fields with leo's data corresponding
+    };
+
+    // println!("{}", leo.email);
+    println!("{}", lea.username);
+    println!("{}", lea.age);
+    println!("{}", lea.email);
+}
+
+struct Person {
+    username: String,
+    email: String,
+    age: usize,
+    is_active: bool,
 }
