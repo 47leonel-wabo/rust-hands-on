3d15ce7fafd6e40caaf67d47b1a80c05fc02e439
leonel47 <waboleonel@gmail.com>
2021-03-01 08:17:21 -0500

    Code refactoring

---
+++ control_flow/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    // Control flow
+    let a = 17;
+
+    if a < 100 {
+        println!("Condition is true");
+    } else {
+        println!("Condition is false");
+    }
+
+    let player_life = 7;
+    let life = if player_life >= 1 { "alive" } else { "died" };
+    println!("{}", life);
+
+    // Loop
+
+}
