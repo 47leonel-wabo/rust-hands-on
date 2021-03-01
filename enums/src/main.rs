55d43b4a863ef7dd747e9e6d71e42f7760365488
leonel47 <waboleonel@gmail.com>
2021-03-01 14:26:59 -0500

    Using match to filter enum value

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -3,6 +3,8 @@ fn main() {
     println!("{:?}", &sally);
 
     println!("is 29 even number: {:?}", is_even(&29));
+
+    user_genre(&sally.genre);
 }
 
 #[derive(Debug)]
@@ -19,4 +21,12 @@ struct Person {
 
 fn is_even(number: &i32) -> Option<bool> {
     if number % 2 == 0 { Some(true) } else { None }
+}
+
+fn user_genre(genre: &Genre) {
+    match genre {
+        Genre::FEMALE => { println!("Female"); }
+        Genre::MALE => { println!("Male"); }
+        _ => { println!("Are you human being?"); } // For anything else
+    }
 }
